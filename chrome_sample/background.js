aafdfbd1f4215087e1a24a82ba7785a8ddddecfa
Cole Foster Conte <cfc009@linuxremote1.bucknell.edu>
2015-11-30 20:53:01 -0500

    moved stuff around

--- chrome_sample/background.js
+++ chrome_sample/background.js
@@ -1,40 +1,19 @@
 var fileName;
 var selected;
-var allfiles;
-var MAXFILELEN = 15; // max length for items in menu
 
 
 var xhr = new XMLHttpRequest();
 //sends a request to the server
 xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    		//JSON.parse does not evaluate the attacker's scripts.
-    		//var resp = JSON.parse(xhr.responseText);
+if (xhr.readyState == 4) {
 	localStorage.setItem("allfiles", xhr.responseText);
-
   	}
 }
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('button').addEventListener('click', clickHandler);
 	document.getElementById("currentFile").innerHTML="Current File: "+localStorage.getItem("filename");
-	var html=populateHTML();
-	for (var x=0; x<html.length; x++){
-		var optString= 'option'+ (x+1).toString();
-		if (html[x].length > MAXFILELEN) { // truncate longer filenames
-		    document.getElementById(optString).outerHTML="<option id="+optString+" label="+html[x].substring(0, MAXFILELEN)+"...>"+html[x]+"</option>";
-		} else { // otherwise, show the whole name
-		    document.getElementById(optString).outerHTML="<option id="+optString+" label="+html[x].substring(0, MAXFILELEN)+">"+html[x]+"</option>";
-		}
-	}
-	while(x<5){
-		var optString= 'option'+ (x+1).toString();
-		document.getElementById(optString).outerHTML="<option id="+optString+" hidden></option>";
-		x++;
-	}	
-
+	
 });
 
 // Set up context menu at install time.
@@ -47,41 +26,11 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 
-xhr.open("GET", "http://brki164-lnx-19.bucknell.edu:9000", true);
-xhr.responseType="text";
-xhr.send();
-
 // add click event
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
-function populateHTML(){	
-    var file = "";
-    var htmlText = "";
-    var count=0;
-    var len=localStorage.getItem("allfiles").length;
-    
-    for (var i = 0; i < len; i++) {
-	if ( (localStorage.getItem("allfiles").charAt(i)) != '\n' ){
-	    file+=localStorage.getItem("allfiles").charAt(i);
-	}
-	else{
-	    htmlText+=file + ",";
-	    file="";
-	    count++;
-	    if (count==5){
-		break;
-	    }
-	}
-    }
-    
-    var list = htmlText.split(",");
-    
-    list.splice(list.length-1, 1);
-    
-    return list;
-}
 
-// The onClicked callback function.
+
 function clickHandler(e) {
 	fileName=document.getElementById("filename").value;
 	if (fileName != ''){
--- chrome_sample/popup.js
+++ chrome_sample/popup.js
@@ -1,40 +1,57 @@
-/*function test(){
-var elem = document.getElementById("test");
-elem.innerHTML = "succeed";
+var allfiles;
+var MAXFILELEN = 15; // max length for items in menu
 var xhr = new XMLHttpRequest();
+//sends a request to the server
 xhr.onreadystatechange = function() {
-if (xhr.readyState == 4) {
-    //JSON.parse does not evaluate the attacker's scripts.
-    //var resp = JSON.parse(xhr.responseText);
-	window.alert(xhr.responseText);
+	if (xhr.readyState == 4) {
+		localStorage.setItem("allfiles", xhr.responseText);
   	}
 }
-xhr.open("POST", "http://brki164-lnx-18.bucknell.edu:9000",true);
-xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xhr.send("hello.txt=testing");
-}*/
-function awesome() {
-  // Do something awesome!
+xhr.open("GET", "http://brki164-lnx-19.bucknell.edu:9000", true);
+xhr.responseType="text";
+xhr.send();
+var html=populateHTML();
+
+for (var x=0; x<html.length; x++){
+	var optString= 'option'+ (x+1).toString();
+	
+	if (html[x].length > MAXFILELEN) { // truncate longer filenames
+		document.getElementById(optString).outerHTML="<option id="+optString+" label="+html[x].substring(0, MAXFILELEN)+"...>"+html[x]+"</option>";
+
+	}
+	else { // otherwise, show the whole name
+		document.getElementById(optString).outerHTML="<option id="+optString+" label="+html[x].substring(0, MAXFILELEN)+">"+html[x]+"</option>";
+	}
 }
+while(x<5){
+	var optString= 'option'+ (x+1).toString();
+	document.getElementById(optString).outerHTML="<option id="+optString+" hidden></option>";
+	x++;
+}	
 
-function totallyAwesome() {
-  // do something TOTALLY awesome!
+
+function populateHTML(){	
+    var file = "";
+    var htmlText = "";
+    var count=0;
+    var len=localStorage.getItem("allfiles").length;
+    
+    for (var i = 0; i < len; i++) {
+	if ( (localStorage.getItem("allfiles").charAt(i)) != '\n' ){
+	    file+=localStorage.getItem("allfiles").charAt(i);
+	}
+	else{
+	    htmlText+=file + ",";
+	    file="";
+	    count++;
+	    if (count==5){
+		break;
+	    }
+	}
+    }
+    
+    var list = htmlText.split(",");
+    
+    list.splice(list.length-1, 1);
+    return list;
 }
-
-function awesomeTask() {
-  awesome();
-  totallyAwesome();
-}
-
-
-
-function main() {
-
-}
-
-// Add event listeners once the DOM has fully loaded by listening for the
-// `DOMContentLoaded` event on the document, and adding your listeners to
-// specific elements when it triggers.
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', clickHandler);
-});
